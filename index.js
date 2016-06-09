c9403f35a5c60e689700703098ddf6da6d519d29
BrianBruneau <Brian8ax@gmail.com>
2016-06-09 16:31:48 -0700

    using MONGO_PROD env variable when deployed

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 var mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/LenderDB');
-console.log("mongo credentials", process.env.MONGO_USER, process.env.MONGO_PASS);
-mongoose.connect("mongodb://"+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@ds011374.mlab.com:11374/lenderspot")
+
+if (process.env.MONGO_USER && process.env.MONGO_PASS) {
+  console.log("mongo credentials", process.env.MONGO_USER, process.env.MONGO_PASS);
+  mongoose.connect("mongodb://"+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@ds011374.mlab.com:11374/lenderspot")
+} else {
+  console.log("mongo credentials", process.env.PROD_MONGODB);
+  mongoose.connect(process.env.PROD_MONGODB);
+}
 
 app.use(express.static(__dirname + '/public/'));
 
